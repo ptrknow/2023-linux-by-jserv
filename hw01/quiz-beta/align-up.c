5a1b39c5d62b477c2d34d8dfe26ef9747e06efec
pachinko <ptrknow@gmail.com>
2023-08-12 16:15:40 +0800

    HW01: quiz beta: fill in missing code and add testing portion
    Compilation:
      gcc align-up.c -o align-up
    
    Run:
      ./align-up
    
    Link: https://hackmd.io/@sysprog/linux2023-summer-quiz0

--- hw01/quiz-beta/align-up.c
+++ hw01/quiz-beta/align-up.c
@@ -1,10 +1,37 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 static inline uintptr_t align_up(uintptr_t sz, size_t alignment)
 {
     uintptr_t mask = alignment - 1;
     if ((alignment & mask) == 0) {  /* power of two? */
-        return MMMM;       
+        return ((sz + mask) & ~mask);       
     }
     return (((sz + mask) / alignment) * alignment);
+}
+
+int main(void)
+{
+    const int set = 4;
+    const int elem = 4;
+    const int sz = set * elem;
+    unsigned long arr[sz];
+    size_t alignment = 4;
+
+    srand(time(0));
+
+    for (int i = 0; i < set; ++i) {
+        unsigned long res = rand() % 999;
+        for (int j = 0; j < elem; ++j)
+            arr[i * set + j] = res + j;
+    }
+
+    printf("Alignment = %lu\n", alignment);
+    for (int i = 0; i < sz; ++i)
+        printf("Test case %4d: before align_up = %4lu, after align_up = %4lu\n",
+            i + 1, arr[i], align_up(arr[i], alignment));
+
+    return 0;
 }
